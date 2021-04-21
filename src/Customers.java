9204eea59c57dc3228905d418e0770a6d49ad73e
Antonin <antonin.sauret@edu.ece.fr>
2021-04-21 20:12:09 +0200

    attribut et constructeur de la classe Customers

--- src/Customers.java
+++ src/Customers.java
@@ -1,6 +1,17 @@
 
 
 public class Customers {
+    //ATTRIBUT ET CONSTRUCTEUR//
+
+    private String panier;
+
+    private String getPanier(){
+        return panier;
+    }
+
+    private void setPanier(String panier){
+        this.panier = panier;
+    }
 
 
 
