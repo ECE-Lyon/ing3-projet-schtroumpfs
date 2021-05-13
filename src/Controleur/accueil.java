8724130961dc99117daa9174b2d6bce09d26a102
Felix <felixlanglois21@gmail.com>
2021-05-13 11:25:08 +0200

    recherche par genre

--- src/Controleur/accueil.java
+++ src/Controleur/accueil.java
@@ -67,4 +67,26 @@ public class accueil {
         }
         return list;
     }
+
+
+    //recherche pour les films sur le genre
+    public ArrayList<String> rechercheGenre(String genre){
+        ArrayList<String> list = new ArrayList<>();
+        try (Connection connection = DriverManager.getConnection("jdbc:h2:./default")){
+            try (PreparedStatement statement = connection.prepareStatement("SELECT IMAGE FROM FILMS WHERE GENRE CONTAINS ?")) {
+                statement.setString(1, genre);
+                try (ResultSet resultSet = statement.executeQuery()){
+                    while (resultSet.next()){
+                        String result = resultSet.getString("IMAGE");
+                        list.add(result);
+                    }
+                }
+
+            }
+        } catch (SQLException exception) {
+            System.out.println("IL Y A EU UNE ERREUR");
+            exception.printStackTrace();
+        }
+        return list;
+    }
 }
